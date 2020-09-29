219704884695ac49bb7f9ab8182fc636bdc1a33d
Victor Cruz <vicc23@gmail.com>
2020-09-29 18:20:36 -0500

    Roles and rolemapping

--- server/server.js
+++ server/server.js
@@ -65,3 +65,27 @@ app.models.user.afterRemote('create', (ctx, user, next) => {
   });
   next();
 });
+
+app.models.Role.find({where: {name: 'admin'}}, (err, role) => {
+  if (!err && role) {
+    console.log('No error, role is', role);
+    if (role.length === 0) {
+      app.models.Role.create({
+        name: 'admin',
+      }, (err2, result) => {
+        if (!err2 && result) {
+          app.models.user.findOne((usererr, user) => {
+            if (!usererr && user) {
+              result.principals.create({
+                principalType: app.models.RoleMapping.USER,
+                principalId: user.id,
+              }, (err3, principal) => {
+                console.log('Created principal', err3, principal);
+              });
+            }
+          });
+        }
+      });
+    }
+  }
+});
